bb8985b79805a84b90ce6bf52bf1078b75fafe24
pedrohenriquebraga <gamesantos7@gmail.com>
2021-05-10 23:50:22 -0300

    Refactor user controller

--- src/services/UploadFiles.ts
+++ src/services/UploadFiles.ts
@@ -14,45 +14,47 @@ export interface UploadedFile {
   url: string;
 }
 
+async function saveInLocal(file: Express.Multer.File, filename: string) {
+  fs.writeFileSync(
+    join(__dirname, "..", "..", "uploads", "files", filename),
+    file.buffer
+  );
+  return {
+    name: filename,
+    url: `${process.env.API_URL}/uploads/${filename}`,
+  };
+}
+
 async function uploadFile({ file, filename, inLocal, path }: UploadFileProps) {
-  if (inLocal) {
-    fs.writeFileSync(
-      join(__dirname, "..", "..", "uploads", "files", filename),
-      file.buffer
-    );
-    return {
-      name: filename,
-      url: `${process.env.API_URL}/uploads/${filename}`,
-    };
-  }
+  if (inLocal) return saveInLocal(file, filename);
+
   const bucket = storage.bucket(process.env.FIREBASE_STORAGE_URL);
-  return new Promise((resolve, reject) => {
-    try {
-      const uploadFile = bucket.file(`${path}/${filename}`);
-      const fileStream = uploadFile.createWriteStream({
-        public: true,
-        contentType: file.mimetype,
-        metadata: {
-          cacheControl:
-            "private, must-revalidate, stale-while-revalidate=60, max-age=600",
-        },
-        gzip: true,
-      });
+  try {
+    const uploadFile = bucket.file(`${path}/${filename}`);
+    const fileStream = uploadFile.createWriteStream({
+      public: true,
+      contentType: file.mimetype,
+      metadata: {
+        cacheControl:
+          "private, must-revalidate, stale-while-revalidate=60, max-age=600",
+      },
+      gzip: true,
+    });
 
-      fileStream.on("finish", () => {
-        const data: UploadedFile = {
-          name: filename,
-          url: uploadFile.publicUrl(),
-        };
+    const uploadedFileInfos = fileStream.on("finish", () => {
+      const data: UploadedFile = {
+        name: filename,
+        url: uploadFile.publicUrl(),
+      };
 
-        resolve(data);
-      });
+      return data;
+    });
+    fileStream.end(file.buffer);
 
-      fileStream.end(file.buffer);
-    } catch (error) {
-      reject(error);
-    }
-  });
+    return uploadedFileInfos;
+  } catch (error) {
+    throw new Error("Error on upload file!");
+  }
 }
 
 export { uploadFile };
