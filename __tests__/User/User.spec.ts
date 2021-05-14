f9fdd8b9a0ace7c2d8f97ba93bd9270017251a0d
pedrohenriquebraga <gamesantos7@gmail.com>
2021-05-13 23:56:29 -0300

    test: add index user test

--- __tests__/User/User.spec.ts
+++ __tests__/User/User.spec.ts
@@ -7,6 +7,7 @@ import { app } from "../../src/http";
 
 describe("Users Tests", () => {
   let connection: Connection;
+  let userID: string;
 
   beforeAll(async () => {
     connection = await createConnection();
@@ -21,25 +22,26 @@ describe("Users Tests", () => {
   it("Should be able to create a user", async () => {
     const response = await request(app)
       .post("/api/users")
-      .send({
-        name: "User Test",
-        email: "user@example.com",
-        password: "Test123",
-        avatar: fs
-          .readFileSync(
-            path.join(
-              __dirname,
-              "..",
-              "..",
-              "uploads",
-              "files",
-              "avatar.test.jpeg"
-            )
-          )
-          .toString("base64"),
-      });
+      .field("name", "User Test")
+      .field("email", "user@example.com")
+      .field("password", "Test123")
+      .attach(
+        "avatar",
+        path.join(__dirname, "..", "..", "uploads", "files", "avatar.test.jpeg")
+      );
+
+    userID = response.body.id;
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("avatar.id");
+  });
+
+  it("Should be able get the created user", async () => {
+    const response = await request(app).get(`/api/users/${userID}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.id).toEqual(userID);
   });
 });
