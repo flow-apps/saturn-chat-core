df77c0ec1aeab1e391a8127b1eb18448209ec3df
pedrohenriquebraga <gamesantos7@gmail.com>
2021-06-03 19:46:20 -0300

    Logger in app error

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,9 +1,12 @@
 export class AppError {
   public readonly statusCode?: number;
   public readonly message: string;
+  private readonly err: Error | any;
 
   constructor(message: string, statusCode = 400) {
     this.message = message;
     this.statusCode = statusCode;
+
+    console.log(this.err);
   }
 }
