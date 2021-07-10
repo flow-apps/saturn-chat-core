f0a3f73c44034fca73a9bc2e9a13484e2514027a
pedrohenriquebraga <gamesantos7@gmail.com>
2021-07-10 15:17:34 -0300

    Separate routes in different files

---
+++ src/routes/group.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import multer from "multer";
+import { configMulter } from "../configs/multer";
+import { GroupsController } from "../controllers/GroupsController";
+import { ParticipantsController } from "../controllers/ParticipantsController";
+import { authProvider } from "../middlewares/authProvider";
+
+const routes = Router();
+const groupsController = new GroupsController();
+const participantsController = new ParticipantsController();
+
+routes.use(authProvider);
+
+routes.post("/groups",
+  multer(configMulter()).single("group_avatar"),
+  groupsController.create
+);
+routes.get("/group/:id", groupsController.index);
+routes.get("/group/participants/new/:group_id", participantsController.new);
+routes.get("/group/participants/list", participantsController.list);
+routes.get("/group/participant/:group_id", participantsController.index);
+routes.get("/groups/list", groupsController.list);
+routes.get("/groups/search", groupsController.search);
+routes.delete("/group/:id", groupsController.delete);
+
+export { routes as groupRoutes };
