5b065c7ebf6ab9c457bc3f448fefa82070807716
pedrohenriquebraga <gamesantos7@gmail.com>
2021-07-12 16:43:08 -0300

    Clear expired invites

--- src/cronjobs/invites.ts
+++ src/cronjobs/invites.ts
@@ -1,12 +1,44 @@
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 import cron from "node-cron"
-import { getCustomRepository, MoreThanOrEqual } from "typeorm"
+import { getCustomRepository } from "typeorm"
 import { InvitesRepository } from "../repositories/InvitesRepository"
 
-const removeExpiredInvitesTask = cron.schedule("1 * * * *", async () => {
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const removeExpiredInvitesTask = cron.schedule("1 * * * * *", async () => {
+  console.log(">> Cleaning all expired invites <<");
+  
+  let deletedAmount = 0
   const invitesRepository = getCustomRepository(InvitesRepository)
-  await invitesRepository.delete({
-    expire_in: MoreThanOrEqual(new Date()),
+  const allInvites = await invitesRepository.find({
+    where: [
+      { is_permanent: false },
+      { is_unlimited_usage: false },
+    ],
+    take: 1000,
+    cache: 1900000,
+    order: { expire_in: 'DESC' }
   })
+
+  Promise.all(allInvites.map(async invite => {
+    if (!invite.expire_in || !invite.expire_timezone) return false;
+
+    const date = new Date();
+    const expireDate = dayjs.tz(invite.expire_in, invite.expire_timezone);
+    const expired = dayjs.tz(date, invite.expire_timezone).isAfter(expireDate);
+    const reachedLimitUsage = invite.usage_amount >= invite.max_usage_amount
+
+    if (expired || reachedLimitUsage) {
+      deletedAmount += 1
+      return await invitesRepository.delete(invite.id)
+    }
+  }))
+
+  console.log(`>> ${deletedAmount} invites expired removed <<`);
+  
 })
 
 export { removeExpiredInvitesTask }
