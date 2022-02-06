f253ca4e52c5f50b36f88b133475c4e322aa771d
pedrohenriquebraga <gamesantos7@gmail.com>
2022-02-06 20:09:13 -0300

    add switch password feature

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
+import { authProvider } from "../middlewares/authProvider";
 import { rateLimiterMiddleware } from "../middlewares/rateLimiter";
 
-const routes = Router()
+const routes = Router();
 const authController = new AuthController();
 
 routes.post("/auth", rateLimiterMiddleware, authController.authenticate);
+routes.patch(
+  "/auth/password/switch",
+  rateLimiterMiddleware,
+  authProvider,
+  authController.switchPassword
+);
 
 export { routes as authRoutes };
