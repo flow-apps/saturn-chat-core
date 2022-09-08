ccb52f797b89191afaa6ddc5a16a48714980bd90
pedrohenriquebraga <gamesantos7@gmail.com>
2022-09-08 17:30:04 -0300

    Add link utils for get website metadata

---
+++ src/utils/link.ts
@@ -0,0 +1,81 @@
+import axios from "axios";
+import { load } from "cheerio";
+
+class LinkUtils {
+  getAllLinksFromText(text: string) {
+    if (!text) {
+      return [];
+    }
+
+    return (
+      text.match(
+        /\b((https?):\/\/|(www)\.)[-A-Z0-9+&@#\/%?=~_|$!:,.;]*[A-Z0-9+&@#\/%=~_|$]/gi
+      ) || []
+    );
+  }
+
+  isValidURL(url: string) {
+    try {
+      new URL(url)
+
+      return true
+    } catch(error) {
+      return false;
+    }
+  }
+
+  transformRelativePathInURL(baseURL: string, path: string) {
+
+    if (!baseURL || !path)
+      return null
+
+    const relativePathRegex = new RegExp('^(?:[a-z+]+:)?//', 'i');
+    const isURL = this.isValidURL(path)
+
+    if (isURL)
+      return path
+
+    const url = new URL(baseURL)
+    const isAbsolutePath = relativePathRegex.test(path);
+
+    if (isAbsolutePath)
+      return path
+
+    const pathWithInitialSlash = path.indexOf("/") === 0 ? path : "/" + path
+
+    return `${url.protocol}//${url.hostname}${pathWithInitialSlash}`
+  }
+
+  async getDataFromLink(link: string) {
+    if (!link) return null;
+
+    const { data, status } = await axios.get(link);
+
+    if (status !== 200) return null;
+
+    const $ = load(data);
+    const siteName =
+      $("head > meta[property='og:site_name']").attr("content") || null;
+    const title =
+      $("head > meta[property='og:title']").attr("content") ||
+      $("head > title").text() ||
+      null;
+    const description =
+      $("head > meta[property='og:description']").attr("content") ||
+      $("head > meta[name='description']").attr("content") ||
+      null;
+    const favicon = $("head > link[rel='icon']").attr("href") || null;
+    const image = $("head > meta[property='og:image']").attr("content") || null;
+
+    return {
+      link,
+      siteName,
+      title,
+      description,
+      favicon: this.transformRelativePathInURL(link, favicon),
+      image: this.transformRelativePathInURL(link, image)
+    };
+  }
+}
+
+export { LinkUtils };
