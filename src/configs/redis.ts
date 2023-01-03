b7b2ddeacb5649356936bd1205481f649c2a27da
pedrohenriquebraga <gamesantos7@gmail.com>
2023-01-02 23:52:11 -0300

    add new node script

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -4,7 +4,8 @@ const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
   username: process.env.REDIS_USERNAME || undefined,
-  password: process.env.REDIS_PASSWORD || undefined
+  password: process.env.REDIS_PASSWORD || undefined,
+  tls: true
 });
 
 redisClient.on("error", err => {
