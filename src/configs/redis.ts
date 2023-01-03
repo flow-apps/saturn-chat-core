8c8f5c79be0d50d8a61b307c1fd86573fad42c2d
pedrohenriquebraga <gamesantos7@gmail.com>
2023-01-02 23:25:39 -0300

    add new redis connection

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -3,6 +3,8 @@ import Redis from "ioredis";
 const redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
+  username: process.env.REDIS_USERNAME || undefined,
+  password: process.env.REDIS_PASSWORD || undefined
 });
 
 redisClient.on("error", err => {
