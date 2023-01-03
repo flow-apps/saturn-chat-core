3aa51f0266cc21a618759b89d83a1c80a3ec5db7
pedrohenriquebraga <gamesantos7@gmail.com>
2023-01-03 00:26:43 -0300

    revert changes

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import FirebaseAdmin from "firebase-admin";
 import path from "path";
 
-const firebaseKeyPath = process.env.FIREBASE_KEY_FILE_PATH || path.join(__dirname, "..", "firebase-key.json")
+const firebaseKeyPath = path.join(__dirname, "..", "firebase-key.json")
 
 FirebaseAdmin.initializeApp({
   credential: FirebaseAdmin.credential.cert(
