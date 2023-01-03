97e0ba8326e06e8525befd7f77877b8b4ef729fe
pedrohenriquebraga <gamesantos7@gmail.com>
2023-01-02 22:27:49 -0300

    fix firebase-key

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,9 +1,12 @@
+import "dotenv/config"
 import FirebaseAdmin from "firebase-admin";
 import path from "path";
 
+const firebaseKeyPath = process.env.FIREBASE_KEY_FILE_PATH || path.join(__dirname, "..", "firebase-key.json")
+
 FirebaseAdmin.initializeApp({
   credential: FirebaseAdmin.credential.cert(
-    require(process.env.FIREBASE_KEY_FILE_PATH || path.join(__dirname, "..", "firebase-key.json"))
+    require(firebaseKeyPath),
   ),
 });
 
