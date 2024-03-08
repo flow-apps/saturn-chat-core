e0b3c29968969cf21d6bc651eaa7f681ef25962d
Pedro Henrique <pedro.braga@wansoft.com.br>
2024-03-08 09:55:04 -0300

    Adicionei senhas no redis

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -2,7 +2,8 @@ import Redis from "ioredis";
 
 const redisClient = new Redis({
   host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT)
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD || undefined
 });
 
 
