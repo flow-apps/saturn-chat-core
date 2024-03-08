5756ccf71200023a50ed94b5b8ebbc4c49f8820b
Pedro Henrique <pedro.braga@wansoft.com.br>
2024-03-08 16:37:22 -0300

    nova mensagem de erro

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,10 +1,12 @@
 export class AppError {
   public readonly statusCode?: number;
   public readonly message: string;
+  public readonly reason?: string;
   private readonly err: Error | any;
 
-  constructor(message: string, statusCode = 400) {
+  constructor(message: string, statusCode = 400, reason = "") {
     this.message = message;
     this.statusCode = statusCode;
+    this.reason = reason
   }
 }
