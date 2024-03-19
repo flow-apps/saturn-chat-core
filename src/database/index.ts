032b298a9436849faa08155d04dbfb18c66e132d
Pedro Henrique <pedro.braga@wansoft.com.br>
2024-03-18 23:21:13 -0300

    Consertei nomes do db

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,10 +3,13 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   const database = {
-    development: process.env.POSTGRES_DATABASE_DEV,
-    prod: process.env.POSTGRES_DATABASE_PROD,
-    test: process.env.POSTGRES_DATABASE_TEST,
+    development: process.env.POSTGRES_DATABASE,
+    prod: process.env.POSTGRES_DATABASE,
+    test: "saturn_chat_test",
   };
+
+  console.log("[Banco de Dados] conectando ao banco:", database[process.env.NODE_ENV]);
+  
 
   const connection = await createConnection(
     Object.assign(defaultOptions, {
