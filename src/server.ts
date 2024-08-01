f33e5027db4fe8b2a8c75c00d42ee1facb02fba1
pedrohenriquebraga <gamesantos7@gmail.com>
2024-08-01 14:45:33 -0300

    Remove clusters

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,11 @@ import "./websockets/group";
 import "./websockets/message";
 import "./websockets/app";
 import { http } from "./http";
-import os from "os";
-import cluster from "cluster";
 
-const totalCPUs = os.availableParallelism();
-
-if (cluster.isPrimary) {
-  console.log(`[ Cluster ] ${totalCPUs} theads disponíveis`);
-  console.log(`[ Cluster ] processo ${process.pid}`);
-
-  for (let i = 0; i < totalCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`[ Cluster ] worker ${worker.process.pid} parou`);
-    console.log("[ Cluster ] criando novo worker");
-    cluster.fork();
-  });
-} else {
-  http.listen(process.env.PORT || 3000, () =>
-    console.log(`Server started in port ${process.env.PORT || 3000} in process ${process.pid}`)
-  );
-}
+http.listen(process.env.PORT || 3000, () =>
+  console.log(
+    `Server started in port ${process.env.PORT || 3000} in process ${
+      process.pid
+    }`
+  )
+);
