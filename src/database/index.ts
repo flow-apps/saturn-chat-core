5d2ad82801ca0ef12211cf4e8a7e38783218222c
pedrohenriquebraga <gamesantos7@gmail.com>
2024-08-02 20:46:56 -0300

    Change connection manager

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,25 +1,28 @@
-import { Connection, createConnection, getConnectionOptions } from "typeorm";
+import {
+  Connection,
+  createConnection,
+  getConnection,
+  getConnectionManager,
+  getConnectionOptions,
+} from "typeorm";
 
 export default async (): Promise<Connection> => {
-  const defaultOptions = await getConnectionOptions();
-  const database = {
-    development: process.env.POSTGRES_DATABASE,
-    prod: process.env.POSTGRES_DATABASE,
-    test: "saturn_chat_test",
-  };
+  let connection: Connection;
 
-  console.log("[Banco de Dados] conectando ao banco:", database[process.env.NODE_ENV]);
-  
+  if (!getConnectionManager().has("default")) {
+    const connectionOptions = await getConnectionOptions();
+    connection = await createConnection(connectionOptions).then((conn) => {
+      console.log("Banco de dados conectado com sucesso");
 
-  const connection = await createConnection(
-    Object.assign(defaultOptions, {
-      database: database[process.env.NODE_ENV],
-    })
-  );
+      return conn;
+    });
+  } else {
+    connection = getConnection();
+  }
 
-  console.log("Conexão com banco de dados estabelecida");
-  
+  await connection.runMigrations().then(() => {
+    console.log("Migrações rodaram com sucesso!");
+  });
 
-  await connection.runMigrations()
-  return connection
+  return connection;
 };
