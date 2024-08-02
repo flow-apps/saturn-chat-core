80e5d8260109d5c6d2c06347bcb580f4cddac880
pedrohenriquebraga <gamesantos7@gmail.com>
2024-08-02 19:57:50 -0300

    fix connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,34 +2,14 @@ import {
   Connection,
   createConnection,
   getConnectionOptions,
-  getConnection,
-  ConnectionNotFoundError,
 } from "typeorm";
 
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
-  try {
-    let connection = getConnection();
-
-    if (!connection.isConnected) {
-      connection = await createConnection(defaultOptions);
-
-      console.log("Conexão com banco de dados estabelecida");
-    }
-
-    await connection.runMigrations().then(() => {
-      console.log("Migrações rodadas com sucesso!");
-    });
-
-    return connection;
-  } catch (error) {
-    const connection = await createConnection(defaultOptions);
-
-    console.log("Conexão com banco de dados estabelecida");
-    await connection.runMigrations().then(() => {
-      console.log("Migrações rodadas com sucesso!");
-    });
-    return connection;
-  }
+  const connection = await createConnection(defaultOptions);
+  await connection.runMigrations().then(() => {
+    console.log("Migrações rodadas com sucesso!");
+  });
+  return connection;
 };
