d462d5c5068c77c77b57d6377726681c19e8c498
pedrohenriquebraga <gamesantos7@gmail.com>
2024-08-13 07:29:22 -0300

    fix storage

--- src/configs/storage.ts
+++ src/configs/storage.ts
@@ -2,7 +2,7 @@ require("dotenv").config({});
 import { Storage } from "@google-cloud/storage";
 import { JWTInput } from "google-auth-library";
 
-const firebaseJsonKey = process.env.FIREBASE_JSON as JWTInput
+const firebaseJsonKey =  JSON.parse(process.env.FIREBASE_JSON) as JWTInput
 
 const storage = new Storage({
   projectId: process.env.FIREBASE_PROJECT_ID,
